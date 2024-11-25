732a62958c6fc7d5051e7c5f069c3b77b6644784
Fotios Galanis <80508822+GFotis@users.noreply.github.com>
2024-11-25 11:53:46 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,17 @@
 from elasticsearch import Elasticsearch
-import pandas as pd
-import tkinter as tk
+import pandas as pd # vivliothiki gia diaxeirisi dedomenon
+import tkinter as tk # vivliothiki gia dimiourgia gui
 
-#es = Elasticsearch("https://localhost:9200", basic_auth=("elastic", "<PBnfeM70JxtCuo0ki2Xw>"))
-es = Elasticsearch(
-    "https://localhost:9200",  # or "https://localhost:9200" if HTTPS is enabled
-    basic_auth=("elastic", "PBnfeM70JxtCuo0ki2Xw"),  # Replace <your-password> with the correct password
-    verify_certs=False
-)
+# Connect to the elasticsearch cluster
+es = Elasticsearch("https://localhost:9200",basic_auth=("elastic", "PBnfeM70JxtCuo0ki2Xw"),verify_certs=False)
 
-# Test the connection
 if es.ping():
     print("Connected to Elasticsearch!")
 else:
     print("Could not connect to Elasticsearch.")
 
-# Create the main application window
+# GUI
 root = tk.Tk()
 root.title("My GUI Application")
 root.geometry("1366x768")  # Width x Height
-
-# Run the Tkinter event loop
 root.mainloop()
