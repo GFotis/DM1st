c6faa38b3282949d9f9c6786751649a3f41765ed
Fotios Galanis <80508822+GFotis@users.noreply.github.com>
2024-12-02 19:10:00 +0200

    Update search.py

--- search.py
+++ search.py
@@ -5,17 +5,47 @@
 import tkinter as tk # Dictionary for the GUI application
 from tkinter import ttk # Dictionary for the GUI application
 from indexing_and_preprocessing import indexing_and_preprocessing
+from elasticsearch_connection import client
+import elasticsearch
 
 def search_menu():
     main_window = tk.Tk()
     main_window.title("Browser \"The Incognitooo...\"")
     main_window.geometry("1366x768")  # Width x Height
+    global frm
     frm = ttk.Frame(main_window, padding=10)
     indexing_and_preprocessing()
     frm.grid()
-    ttk.Label(frm, text="Θέλω αρχικά να επιλέξεις το μοντέλο με το οποίο θα γίνεται η αναζήτησή σου:").grid(column=0, row=0)
-    #ttk.OptionMenu(frm, "Επιλογή μοντέλου", "Μοντέλο 1", "Μοντέλο 2", "Μοντέλο 3").grid(column=1, row=2)
-    ttk.Label(frm, text="Επιλέξτε μία από τις διαθέσιμες επιλογές του φυλλομετρητή: ").grid(column=0, row=1)
-    ttk.Button(frm, text="Έξοδος ", command=main_window.destroy).grid(column=1, row=0)
-    ttk.Button(frm, text="Αναζήτηση ", command=search_menu).grid(column=1, row=0)
+    ttk.Label(frm, text="Επέλεξε με ποιο μοντέλο θέλεις να γίνει η αναζήτηση:").grid(column=0, row=0)
+    ttk.Button(frm, text="Boolean Model ", command=searching_boolean).grid(column=1, row=0)
+    ttk.Button(frm, text="VSM Model ", command=searching_vsm).grid(column=2, row=0)
+    ttk.Button(frm, text="Έξοδος", command=main_window.destroy).grid(column=3, row=0)
     main_window.mainloop()
+
+def searching_vsm():
+    resp = client.search(index="l ast_statement", query={"match_all": {}})
+    for hit in resp['hits']['hits']:
+        print(f"Αναζήτηση: {hit['_source']}")
+
+
+def searching_boolean():
+    ttk.Label(frm, text="Εισάγετε το κείμενο που θέλετε να αναζητήσετε:").grid(column=0, row=1)
+    search_entry = ttk.Entry(frm, width=50)
+    
+    query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"match": {"LastStatement": fields["must"]}}  # Must match 'must' term in LastStatement
+                ],
+                "should": [
+                    {"match": {"FirstName": fields["should"]}},  # Should match 'should' term in FirstName
+                    {"match": {"LastName": fields["should"]}}   # Should match 'should' term in LastName
+                ],
+                "must_not": [
+                    {"match": {"LastName": fields["must_not"]}}  # Must not match 'must_not' term in LastName
+                ]
+            }
+        }
+    }
+    pass
